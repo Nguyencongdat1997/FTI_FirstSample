b0d1d1c7e6fea6ebb51b879e245fc19db5225b51
DatGatto <nguyencongdat1998@gmail.com>
2018-09-04 14:49:09 +0700

    FirstSample | Add some feature to post data when saving

--- facebookapi_connector.py
+++ facebookapi_connector.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class FacebookAPIConnector(object):	
 
 	def __init__(self):
--- post_crawler.py
+++ post_crawler.py
@@ -2,6 +2,7 @@ from facebookapi_connector import FacebookAPIConnector
 from mongodb_connector import UserDAO,PostDAO,CommentDAO
 from file_saver import PostSaver
 import json
+import datetime
 
 facebookapi_connector = FacebookAPIConnector()
 user_dao = UserDAO()
@@ -14,7 +15,7 @@ for uid in user_ids:
 	user_posts = facebookapi_connector.get_posts_of_one_user(uid)
 	for post_id in user_posts:
 		post = user_posts[post_id]
-		data_to_save = dict(post_id= post_id, message= post[0], creator_id= post[1])		
-		post_saver.write_to_file(json.dumps(data_to_save, ensure_ascii=False, encoding='utf-8'))
+		data_to_save = dict(post_id = post_id, user_id = uid, message = post[0], creator_id= post[1], time_stamp = str(datetime.datetime.now()))		
+		post_saver.write_to_file(json.dumps(data_to_save, ensure_ascii = False, encoding = 'utf-8'))
 		count += 1
 		print (str(count) + ':' + post_id)
